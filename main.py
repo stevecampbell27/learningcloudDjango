108ba5703031f0df29f8e8ddb4ded70262d0421f
Steven Campbell <steven.campbell@skiff.com>
2023-05-15 14:46:14 +0000

    c

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app = Flask(__name__)
 def hello_world():
     name = os.environ.get("NAME", "wow")
     return f"Hello {name}!"
+    return f"CUNT"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
